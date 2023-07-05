5d5cc871b6553d0aef7ea1df9b593f7db93c61bb
timeyimi <844966445@qq.com>
2023-07-05 16:56:58 +0800

    changes to lab1

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -205,6 +205,11 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 
 		// (unsigned) octal
 		case 'o':
+			// 从ap指向的可变字符串中获取输出的值
+			num = getuint(&ap, lflag);
+			// 设置基数为8
+			base = 8;
+			goto number;
 			// Replace this with your code.
 			putch('X', putdat);
 			putch('X', putdat);
